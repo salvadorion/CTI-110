247759fa4b5f35f0658c56108b7c64e0a24a9fde
salvadorion <leifur225@gmail.com>
2024-11-12 16:33:59 +0000

    adding value-returning functions

--- function_example.py
+++ function_example.py
@@ -4,7 +4,17 @@
 def my_animal(name, sound, pound_food):
     print (f"The {name} makes a {sound} noise!")
     print (f"The {name} eats {pound_food} pounds of food a day!")
-    print (f"The {name} eats {pound_food * 7} pounds of food a week!")
+    print (f"The {name} eats {(pound_food * 7):.2f} pounds of food a week!")
+
+# Define a value returning function
+def getName():
+    name = input("Enter your name: ")
+    return name
+
+def displayName(first):
+    lastName = input("Enter your Last name: ")
+    fullName = first + " " + lastName
+    return fullName
 
 # Create a main function - all logic goes here
 def main():
@@ -12,6 +22,14 @@ def main():
     print()
     # Call the my_animal band
     my_animal("Lion", "roar", 20.5)
+    print()
+    my_animal("Mouse", "squeak", 0.2)
+
+    # Call the value-returning function
+    myName = getName()
+    print(myName)
+    print()
+    print(displayName(myName))
 
 # Call the main function
 if __name__ == "__main__":
